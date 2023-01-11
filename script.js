5cc38bb1220e61718333692d814b763936976185
MateusGSouza <115435321+MateusGSouza@users.noreply.github.com>
2023-01-11 17:33:32 -0300

    Add functions to get the computer and player choices and play a round of Rock, Paper, Scissors

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log("Hello?")
+const choices = ["rock", "paper", "scissors"] ;
+
+function getComputerChoice(choices) {
+    computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    return  computerChoice
+};
+
+function getPlayerChoice(choices) {
+    const playerChoice = prompt("Rock, Paper or Scissors? ");
+    if (choices.includes(playerChoice.toLowerCase()) == false) {
+        console.log(`Choice "${playerChoice}" is invalid. Please, select one of the following: ${choices}`)
+    }
+    return playerChoice.toLowerCase()
+};
+
+function singleRound(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case (playerSelection == computerSelection):
+            return "Draw"
+            break;
+
+        case (playerSelection == "rock" && computerSelection == "scissors"):
+            return "Player wins! Rock beats scissors!"
+            break;
+
+        case (playerSelection == "rock" && computerSelection == "paper"):
+            return "Computer wins! Paper beats rock!"
+            break;
+
+        case (playerSelection == "paper" && computerSelection == "rock"):
+            return "Player wins! Paper beats rock!"
+            break;
+
+        case (playerSelection == "paper" && computerSelection == "scissors"):
+            return "Computer wins! Scissors beats paper!"
+            break;
+
+        case (playerSelection == "scissors" && computerSelection == "paper"):
+            return "Player wins! Scissors beats paper!"
+            break;
+
+        case (playerSelection == "scissors" && computerSelection == "rock"):
+            return "Computer wins! Rock beats scissors!"
+            break;
+    }
+}
